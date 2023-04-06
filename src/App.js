7f6e1cb1fe46787988c8eb8fc96e19eb79a58f8b
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-04-07 02:38:09 +0500

    Set up the project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import BookList from './components/BookList';
+import Categories from './components/Categories';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<BookList/>} />
+        <Route path="/categories" element={<Categories />}/>
+      </Routes>
+    </>    
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './header.css'
+
+function Header() {
+  return (
+    <header className='header'>
+        <h1>Bookstore CMS</h1>
+        <ul>
+            <li>
+                <NavLink to="/">BOOKS</NavLink>
+            </li>
+
+            <li>
+                <NavLink to="/categories">CATEGORIES</NavLink>
+            </li>
+        </ul>
+    </header>
+
+  )
+}
+
+export default Header
